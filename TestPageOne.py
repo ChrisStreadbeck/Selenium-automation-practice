cebd1c4207cbabde99e2fa26e83aa68306407ddf
ChrisStreadbeck <han0987@gmail.com>
2019-09-09 21:34:38 -0600

    first commit

---
+++ TestPageOne.py
@@ -0,0 +1,56 @@
+from selenium import webdriver
+import os
+
+class TestPageOne():
+
+    def test(self):
+        baseUrl = "https://learn.letskodeit.com/p/practice"
+        driverLocation = "chromedriver.exe"
+        os.environ["webdriver.chrome.driver"] = driverLocation
+        driver = webdriver.Chrome(driverLocation)
+        driver.get(baseUrl)
+        driver.maximize_window()
+
+        elementById = driver.find_element_by_id("name")
+
+        if elementById is not None:
+            print("We found an element by ID")
+
+        elementByName = driver.find_element_by_name("enter-name")
+
+        if elementByName is not None:
+            print("We found an element by Name")
+
+        elementByXPath = driver.find_element_by_xpath("//input[@id=\"name\"]")
+
+        if elementByXPath is not None:
+            print("We found an element by XPath")
+
+        elementByCSS = driver.find_element_by_css_selector("#displayed-text")
+
+        if elementByCSS is not None:
+            print("We found an element by CSS")
+
+        elementByLinkText = driver.find_element_by_link_text("Login")
+
+        if elementByLinkText is not None:
+            print("We found an element by Link Text")
+
+        elementByPartialLinkText = driver.find_element_by_partial_link_text("Logi")
+
+        if elementByPartialLinkText is not None:
+            print("We found an element by Partial Link Text")
+
+        elementByClassName = driver.find_element_by_class_name("inputs")
+
+        if elementByClassName is not None:
+            print("We found an element by Class")
+                    
+        elementByTagName = driver.find_element_by_tag_name("h1")
+
+        if elementByTagName is not None:
+            print("We found an element by Tag")
+
+
+chromeTest = TestPageOne()
+chromeTest.test()
